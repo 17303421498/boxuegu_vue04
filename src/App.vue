effe6af8017222fe4cba943995459619e4045804
xl <1558655926@qq.com>
2022-11-15 16:25:17 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <MyButton></MyButton>
+  </div>
+</template>
+
+<script>
+import MyButton from '@/components/MyButton.vue'
+export default {
+  components: {
+    MyButton
+  }
+}
+</script>
+
+<style>
+</style>
